4a9a2038f36676ac841d95a3a9fc630cf034cc20
Porter Laartz <plaartz@wisc.edu>
2023-01-17 17:15:31 -0600

    Updates

---
+++ precache-manifest.bda1a3f31968bed18da17460e5fad77a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfbd88ceafa848fc3ba1",
+    "url": "/static/css/main.de69ae66.chunk.css"
+  },
+  {
+    "revision": "bfbd88ceafa848fc3ba1",
+    "url": "/static/js/main.bfbd88ce.chunk.js"
+  },
+  {
+    "revision": "7cde43511c48cae86c94",
+    "url": "/static/js/1.7cde4351.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9bf560c28cda412e16e837821ec7a7ec",
+    "url": "/index.html"
+  }
+];
